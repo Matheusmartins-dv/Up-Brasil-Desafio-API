47727228b32521f5675bb0c51ad1233798656606
matheusmartinsInsper <matheusmartinsInsper>
2025-12-11 04:28:33 -0300

    Adiciona .gitignore e remove arquivos de build rastreados

--- Domain/Constants/DefaultValuesDomainConstants.cs
+++
@@ -1,6 +0,0 @@
-namespace Domain.Constants;
-
-public static class DefaultValuesDomainConstants
-{
-    public const string DefaultPassword = "upBrasil123#";
-}
--- Domain/Interfaces/IUserValidationService.cs
+++
@@ -1,8 +0,0 @@
-namespace Domain.Interfaces;
-
-public interface IUserValidationService
-{
-   Task ValidateUniqueness(string email, string document, CancellationToken cancellationToken);
-   Task ValidateUniquenessEmail(string email, CancellationToken cancellationToken);
-   Task ValidateUniquenessDocument(string document, CancellationToken cancellationToken);
-}
